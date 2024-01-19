70d97f094e2a19870b111a0a39b46091f42beae6
AltCrest <149634517+AltCrest@users.noreply.github.com>
2024-01-18 20:42:16 -0500

    change "Products" to "Projects" using javascript

--- scripts/app.js
+++ scripts/app.js
@@ -3,3 +3,7 @@
  * app.js
  * 
  */
+
+window.onload = function() {
+    document.getElementById("products").innerHTML = "<a class='nav-link' href='#' id='products'><span class='fa fa-th'></span> Projects</a>";
+}
