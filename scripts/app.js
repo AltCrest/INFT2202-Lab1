29e08ff60d0f82f032038b59b7eab33fa59ffb26
AltCrest <149634517+AltCrest@users.noreply.github.com>
2024-01-31 18:06:57 -0500

    Add human resources link between about us and contact us(can be improved)

--- scripts/app.js
+++ scripts/app.js
@@ -5,5 +5,9 @@
  */
 
 window.onload = function() {
+    document.getElementsByTagName("body")[0].innerHTML += "<p>Welcome</p>";
     document.getElementById("products").innerHTML = "<a class='nav-link' href='#' id='products'><span class='fa fa-th'></span> Projects</a>";
+    document.getElementsByTagName("li")[3].innerHTML = "<a class='nav-link' href='#'><span class='fa fa-info'></span> About Us</a> <li class='nav-item'><a class='nav-link' href='#'><span class='fa fa-users'></span> Human Resources</a></li>";
+    document.getElementsByTagName("body")[0].innerHTML += "<nav class='navbar fixed-bottom navbar-light bg-light'><p>&copy; CopyRight 2024</p></nav>"
+
 }
