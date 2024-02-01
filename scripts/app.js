ee3277dda7f445d58a822c8ab575b8ae14046db3
AltCrest <149634517+AltCrest@users.noreply.github.com>
2024-01-31 19:25:00 -0500

    added onClick() function
    - shows what user typed to the console
    - redirect to index after 3 second

--- scripts/app.js
+++ scripts/app.js
@@ -10,9 +10,37 @@ window.onload = function() {
     document.getElementsByTagName("li")[3].innerHTML = "<a class='nav-link' href='#'><span class='fa fa-info'></span> About Us</a> <li class='nav-item'><a class='nav-link' href='#'><span class='fa fa-users'></span> Human Resources</a></li>";
     document.getElementsByTagName("body")[0].innerHTML += "<nav class='navbar fixed-bottom navbar-light bg-light'><p>&copy; CopyRight 2024</p></nav>"
 
+    // let form = document.getElementById("name");
+    // form.addEventListener("submit", (e) => {
+    //     e.preventDefault();
+    //     console.log("Form Submitted!");
+    // })
 }
 
-function submitClick() {
 
+
+
+// function getName(){
+//     console.log(document.getElementsByID("name").innerText)
+// }
+
+function submitClick() {
+    const form = document.getElementById("myForm");
+ 
+    form.addEventListener("submit", function (e) {
+        e.preventDefault(); // Prevent form submission
+ 
+        // Get user input values
+        const name = document.getElementById("name").value;
+        const contactNumber = document.getElementById("contactNumber").value;
+ 
+        // Log user information to console
+        console.log(`User submitted: Name: ${name}, contactNumber: ${contactNumber}`);
+ 
+        // Start a timer to redirect after 3 seconds
+        setTimeout(() => {
+            window.location.href = "index.html"; // Replace with your home page URL
+        }, 3000);
+    });
     
 }
