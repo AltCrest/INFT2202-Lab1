b71b97243979969567735b45e8489d2a09bd6aa4
AltCrest <149634517+AltCrest@users.noreply.github.com>
2024-02-02 12:53:20 -0500

    Fixed the welcome message to display on top of page cleaned up the submitClick() function added comments

--- scripts/app.js
+++ scripts/app.js
@@ -1,46 +1,43 @@
 /**
- * John Barrios
+ * John Barrios(100821013)
  * app.js
- * 
+ * 02/02/24
  */
 
 window.onload = function() {
-    document.getElementsByTagName("body")[0].innerHTML += "<p>Welcome</p>";
-    document.getElementById("products").innerHTML = "<a class='nav-link' href='#' id='products'><span class='fa fa-th'></span> Projects</a>";
-    document.getElementsByTagName("li")[3].innerHTML = "<a class='nav-link' href='#'><span class='fa fa-info'></span> About Us</a> <li class='nav-item'><a class='nav-link' href='#'><span class='fa fa-users'></span> Human Resources</a></li>";
-    document.getElementsByTagName("body")[0].innerHTML += "<nav class='navbar fixed-bottom navbar-light bg-light'><p>&copy; CopyRight 2024</p></nav>"
+    // display a welcome page to the website
+    var navbar = document.querySelector(".navbar");
+    navbar.insertAdjacentHTML('afterend', '<div><h2>Welcome To the Page</h2></div>');
 
-    // let form = document.getElementById("name");
-    // form.addEventListener("submit", (e) => {
-    //     e.preventDefault();
-    //     console.log("Form Submitted!");
-    // })
+    // Change Products link to Projects
+    document.getElementById("products").innerHTML = "<a class='nav-link' href='#' id='Projects'><span class='fa fa-th'></span> Projects</a>";
+
+    // added human resources between About Us and Contact Us
+    document.getElementsByTagName("li")[3].innerHTML = "<a class='nav-link' href='#'><span class='fa fa-info'></span> About Us</a> <li class='nav-item'><a class='nav-link' href='#'><span class='fa fa-users'></span> Human Resources</a></li>";
+    
+    // added a fixed bottom from copyright text
+    document.getElementsByTagName("body")[0].innerHTML += "<nav class='navbar fixed-bottom navbar-light bg-light'><p>&copy; CopyRight 2024</p></nav>"
 }
 
+/**
+ * Handles the submission of click event
+ * @param {Event} event 
+ */
+function submitClick(event) {
+    // prevent default submission
+    event.preventDefault();
 
+    // Get user input values
+    const name = document.getElementById("name").value;
+    const contactNumber = document.getElementById("contactNumber").value;
+    const email = document.getElementById("email").value;
+    const msg = document.getElementById("message").value;
 
+    // Log user information to console
+    console.log(`Name: ${name}\ncontactNumber: ${contactNumber}\nEmail: ${email}\nUser Message: ${msg}`);
 
-// function getName(){
-//     console.log(document.getElementsByID("name").innerText)
-// }
-
-function submitClick() {
-    const form = document.getElementById("myForm");
- 
-    form.addEventListener("submit", function (e) {
-        e.preventDefault(); // Prevent form submission
- 
-        // Get user input values
-        const name = document.getElementById("name").value;
-        const contactNumber = document.getElementById("contactNumber").value;
- 
-        // Log user information to console
-        console.log(`User submitted: Name: ${name}, contactNumber: ${contactNumber}`);
- 
-        // Start a timer to redirect after 3 seconds
-        setTimeout(() => {
-            window.location.href = "index.html"; // Replace with your home page URL
-        }, 3000);
-    });
-    
+    // Start a timer then redirect to HomePage after 3 seconds
+    setTimeout(() => {
+        window.location.href = "index.html"; 
+    }, 3000);
 }
