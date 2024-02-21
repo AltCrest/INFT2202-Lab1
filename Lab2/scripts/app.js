61406ec35e290cb4ce2c28112c5f04f72f1341d8
AltCrest <149634517+AltCrest@users.noreply.github.com>
2024-02-21 17:52:57 -0500

    Connected the links

--- Lab2/scripts/app.js
+++ Lab2/scripts/app.js
@@ -7,8 +7,6 @@
 
 window.onload = function() {
     // display a welcome page to the website
-    var navbar = document.querySelector(".navbar");
-    navbar.insertAdjacentHTML('afterend', '<div><h2>Welcome To the Page</h2></div>');
 
     // Change Products link to Projects
     document.getElementById("products").innerHTML = "<a class='nav-link' href='#' id='Projects'><span class='fa fa-th'></span> Projects</a>";
@@ -42,7 +40,6 @@ function submitClick(event) {
         window.location.href = "index.html"; 
     }, 3000);
 }
-
 class Contact
 {
     constructor(contactName = "", emailAddress = "", contactNumber = "", contactMessage = "")
@@ -53,3 +50,210 @@ class Contact
         this.contactMessage = contactMessage;
     }
 }
+
+"use strict";
+//IIFE - Immediately Invoked Function Expression
+// mean? -> anonymous self-executing function
+
+let app;
+(function(app){
+
+    // Declare Function Variables here...
+    console.log("%cDeclaring Variables", "color: red;")
+    let contactObject = new Contact();
+
+    /**
+     * Variable initialization in this function
+     *
+     */
+    function Start()
+    {
+       PageSwitcher();   
+
+        Main();
+    }
+
+    function PageSwitcher()
+    {
+        let name = window.location.pathname;
+
+        let pageName = name.substring(1, name.length - 5);
+
+        switch(pageName)
+            {
+                case "index":
+                DisplayHomePageContent();
+                    break;
+                case "login":
+                    DisplayLoginContent();
+                    break;
+                case "register":
+                    DisplayRegisterContent();
+                    break;
+                default:
+                    console.error("Landed in a page that is not defined");
+                    break;
+            }
+
+            // add a class of active to the active link
+        $("#"+pageName).addClass("active");
+    }
+
+    function DisplayHomePageContent()
+    {
+        var navbar = document.querySelector(".navbar");
+        navbar.insertAdjacentHTML('afterend', '<div><h2>Welcome To the Page</h2></div>');
+    }
+
+    /**
+     * display content and finctions for contact page
+     */
+    function DisplayLoginContent()
+    {
+        function clearForm()
+        {
+            //document.getElementById("contactForm").reset();
+            $("#contactForm")[0].reset();
+            $('#errorMessage').hide();
+        }
+        
+        function ValidateInput(selector, condition, errorMessage) {
+            if (condition) {
+                // if failed
+                $("#errorMessage").show();
+                $("#errorMessage").text(errorMessage);
+                $(selector).select();
+                $(selector).css("border", "2px solid red");
+            } else {
+                $('#errorMessage').hide();
+                $(selector).css("border", "1px solid green");
+            }
+        }
+
+        $("#contactName").change((e)=>
+        {
+            if( $("#contactName").length < 8)
+            {
+                console.log("Contact Name Too Short");
+            }
+            console.log("changed");
+        });
+
+        $("#contactName").blur((e)=>
+        {
+            validateInput("#contactName", ($("#contactName").val().length < 8), "Contact name is too short")
+        });
+
+        $("#contactName").focus((e)=>
+        {
+            $("#contactName").select();
+        });
+
+        $("#emailAddress").blur((e) => {
+            validateInput("#emailAddress", ($("#emailAddress").val().length < 8 ||(!$("#emailAddress").include("@"))), "Invalid Email Address")
+        })
+
+        $("#emailAddress").focus((e)=>
+        {
+            $("#emailAddress").select();
+        });
+
+        $("#contactNumber").blur((e) => {
+            let phoneNumberPatter = /^(\+\d{1,2}\s)?\(?\d{3}\)?[\s.-]\d{3}[\s.-]\d{4}$/;
+            let phoneNumber = $("#contactNumber").val();
+
+            validateInput("#contactNumber", (!phoneNumberPatter.test(phoneNumber)), "Invalid number, please use the pattern (999)-999-9999");
+        })
+
+        $("#contactNumber").focus((e)=>
+        {
+            $("#contactNumber").select();
+        });
+
+        $("#contactMessage").blur((e) => {
+            validateInput("#contactMessage", ($("#contactMessage").val().length < 2), "Message is to short");
+        })
+
+        $("#contactMessage").focus((e)=>
+        {
+            $("#contactMessage").select();
+        });
+
+        $("#contactForm").submit((e)=>
+        {
+            if (document.getElementById("contactForm").checkValidity() == false) {
+                e.preventDefault();
+            }
+            let contactName = $("#contactName").val();
+            let emailAddress = $("#emailAddress").val();
+            let contactNumber = $("#contactNumber").val();
+            let contactMessage = $("#contactMessage").val();
+
+            console.log(`Contact Name: ${contactName}`);
+            console.log(`Email Address: ${emailAddress}`);
+            console.log(`Contact Number: ${contactNumber}`);
+            console.log(`Contact Message: ${contactMessage}`);
+
+            contactObject.contactName = contactName;
+            contactObject.emailAddress = emailAddress;
+            contactObject.contactNumber = contactNumber;
+            contactObject.contactMessage = contactMessage;
+
+            console.log(contactObject);
+
+            clearForm();
+        });
+        $("#submitButton").click((e)=>
+        {
+            e.preventDefault();
+            let contactName = $("#contactName").val();
+            let emailAddress = $("#emailAddress").val();
+            let contactNumber = $("#contactNumber").val();
+            let contactMessage = $("#contactMessage").val();
+
+            console.log(`Contact Name: ${contactName}`);
+            console.log(`Email Address: ${emailAddress}`);
+            console.log(`Contact Number: ${contactNumber}`);
+            console.log(`Contact Message: ${contactMessage}`);
+
+            contactObject.contactName = contactName;
+            contactObject.emailAddress = emailAddress;
+            contactObject.contactNumber = contactNumber;
+            contactObject.contactMessage = contactMessage;
+
+            console.log(contactObject);
+
+            clearForm();
+        });
+
+        $("#resetButton").click((e)=>
+        {
+            e.preventDefault();
+            if(confirm("Are You Sure?"))
+            {
+                clearForm();
+            }
+
+            
+        });
+    }
+
+    function DisplayRegisterContent()
+    {
+
+    }
+
+    /**
+     * Main Program entry point is here
+     *
+     */
+    function Main()
+    {
+       
+    }
+    
+    
+
+    window.addEventListener("load", Start);
+})(app || (app = {}));
+
